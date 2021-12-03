477cdd09b358f90135bed69de536b4aa0335e13b
Daniele-Simonetti <daniele.simonetti99@outlook.it>
2021-12-03 16:25:08 +0100

    testing functions

--- js/script.js
+++ js/script.js
@@ -4,42 +4,41 @@
 // Dopo 30 secondi l’utente deve inserire, uno alla volta, i numeri che ha visto precedentemente, tramite il prompt().
 // Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati.
 
+// INSERISCO LA FUNZIONE DI MATHRANDOM
+function getRndInteger(min, max) {
+  return Math.floor(Math.random() * (max - min + 1) ) + min;
+}
+
+// FUNZIONE PER CREARE I CONTAINER
+function createDiv(randomNum) {
+  // seleziono il mio container per creargli all'interno i miei div
+  document.getElementById('container');
+  // per ogni numero nel mio array creo un div nel mio html che lo contenga
+  const numContainer = document.createElement('div');
+  // all'interno del mio div inserisco uno dei miei random num
+  numContainer.innerHTML = randomNum;
+  // console.log(numContainer);
+  // appendo i miei div al container precedentemente selezionato
+  main_container.append(numContainer);
+
+  return numContainer;
+}
+// /FUNZIONE PER CREARE I CONTAINER
+
+
 // 1. creo un array in cui pushare 5 numeri random
 const arrayNumber = [];
 console.log('array vuoto', arrayNumber);
 
-// seleziono il mio container per creargli all'interno i miei div
-document.getElementById('container');
 // genero 5 numeri random unici
 while (arrayNumber.length < 5) {
-  let randomNum = Math.floor(Math.random() * 10) + 1;
+  let randomNum = getRndInteger(1, 5); 
   if (arrayNumber.includes(randomNum)) {
     
-  } else {
+  } else { //se non è presento nell'array
     arrayNumber.push(randomNum)
-    // per ogni numero nel mio array creo un div nel mio htmlche lo contenga
-    const numContainer = document.createElement('div');
-    // all'interno del mio div inserisco uno dei miei random num
-    numContainer.innerHTML = randomNum;
-    console.log(numContainer);
-    // appendo i miei div al container precedentemente selezionato
-    main_container.append(numContainer);
+    let element = createDiv(randomNum);
   }
 }
 console.log('array con numeri random', arrayNumber);
 
-
-// // creo nell'html un ul per contenere i miei li
-// // seleziono la mia ul
-// const list = document.querySelector('.list-box');
-
-// // creo tanti li nel mio html quanti sono i numeri randomici
-// for (let i = 0; i < arrayNumber.length; i++) {
-//   let list_item = document.createElement('li');
-//   // console.log(list_item);
-//   // appendo gli li alla mia ul
-//   list.appendChild(list_item);
-//   // inserisco nei miei li i numeri all'interno del mio array
-//   list_item.innerHTML = arrayNumber;
-// }
-// console.log(list);
